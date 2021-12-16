a7fd7ba799edc4ce37aa52f50f8cfb42757a9685
“GuardianesdelaNoche” <equipoguardianesdelanoche@gmail.com>
2021-12-16 20:05:06 +0100

    First Repo

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const port = 3005
+const IP_SERVER = process.env.IP_SERVER;
+
+app.get('/', (req, res) => {
+  res.send(`Hello World from ${IP_SERVER}!`)
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
